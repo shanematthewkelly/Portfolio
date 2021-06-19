443977cc04463293db63222d08a98f2590235f83
BuildTools <unconfigured@null.spigotmc.org>
2021-06-19 18:58:41 +0100

    Spline Integration & Rough UI Setup

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 <p>1. Clone this repository and install the required dependencies by running <code>npm install</code></p>
 
-<p>2. <code>cd</code> into the project directory and run <code>gatsby develop</code> to start the dev server on localhost:3000.</p>
+<p>2. <code>cd</code> into the project directory and run <code>gatsby develop</code> to start the dev server on localhost:8000.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,14 @@ import '../styles/layout.scss'
 const IndexPage = () => (
   <>
     <div className="centered-container">
-      <h1 className="rich-text">Hi
-        <span className="rich-text-2">, my name is Shane.</span></h1>
+      <div className="hide"></div>
+      <iframe src='https://my.spline.design/untitled-b29297043a1c6af210ecd66f1992ec99/' frameborder='0' width='100%' height='100%'></iframe>
+
+      <div id="content">
+        <h1 className="rich-text">Hi
+          <span className="rich-text-2">, my name is Shane.</span></h1>
+        <h3 className="desc-header">[ Drag mouse to rotate ]</h3>
+      </div>
     </div>
   </>
 )
